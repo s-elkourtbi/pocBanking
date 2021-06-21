3df7baf09ba534dd6efb283f53044d9f8b469684
s.el-kourtbi <s.elkourtbi@gmail.com>
2021-06-21 13:09:29 +0200

    first commit of kata

---
+++ src/test/java/com/banking/gestionDeCompte/GestionDeCompteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking.gestionDeCompte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeCompteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
